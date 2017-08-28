ea3d3e02eae481079da68c156a7a2234ef380411
Klim RIl <klimril@gmail.com>
2017-08-28 17:11:36 +0700

    start-over

---
+++ widget/src/components/start-over/start-over.vue
@@ -0,0 +1,3 @@
+<template src="./start-over.html"></template>
+<style lang="less" src="./start-over.less"></style>
+<script src="./start-over.js"></script>
--- widget/src/components/step-finish/step-finish.js
+++ widget/src/components/step-finish/step-finish.js
@@ -1,8 +1,10 @@
 import Help from '../help/help.vue';
+import StartOver from '../start-over/start-over.vue';
 
 export default {
   components: {
     Help,
+    StartOver,
   },
   methods: {
     handlerReset() {
